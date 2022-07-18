6bbb9cfc314555b52a816be3379564a870b2c250
ben <isieldor@gmail.com>
2022-07-18 16:32:03 -0500

    before emoji mart add

--- src/App.tsx
+++ src/App.tsx
@@ -20,32 +20,15 @@ function App() {
   ]);
 
   /*
-  old values:
-  311 and 17.388 = predicted 17, actual 28 | 311 / 28 = 11.107
-  580 and 23, predicted 23.9 actual 41 | 580 / 41 = 14.146
-  1090 and 43.2, predicted 25.239 actual 42 = 25.95
-
-  new values:
-  width: 227, font size 11.2, predicted 20.27, actual 33
-  width: 282.766, font size: 13.65, predicted: 20.715, actual 34
-  width: 573.5, font size 26.88, predicted 21, actual 35
-  width: 845.76, font size 39.27, predicted 21 actual 36
+  width to # characters before input wrap
+  360 : 27, wrap on 28
+  414: 32, wrap on 33
+  540: 45, wrap on 46
   */
   const [newInputVal, setNewInputval] = useState("");
-  const [inputFontSize, setInputFontSize] = useState(0);
-  const [inputWidth, setInputWidth] = useState(0);
-  const [predictedChars, setPredictedChars] = useState(0);
+  const [inputRows, setInputRows] = useState(1);
 
   const formInputRef = useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-
-    handleResize();
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-  // const formInput = document.getElementById("newBulletInput");
-  const [inputRows, setInputRows] = useState(1);
 
   const onNewBulletSubmit = () => {
     if (newInputVal) {
@@ -56,25 +39,6 @@ function App() {
       };
       setBullets([...bullets, newBullet]);
       setNewInputval("");
-    }
-  };
-
-  const handleResize = () => {
-    const formInput = formInputRef.current;
-    if (formInput) {
-      const formInputStyle = window.getComputedStyle(formInput);
-      const rawInputSize = formInputStyle.fontSize;
-      const rawInputWidth = formInputStyle.width;
-      const calcFontSize = Number(rawInputSize.slice(0, -2));
-      const calcInputWidth = Number(rawInputWidth.slice(0, -2)) - 16;
-      setInputFontSize(calcFontSize);
-      setInputWidth(calcInputWidth);
-
-      const predictedChars = Math.round(
-        0.50187 * calcInputWidth - 11.02113 * calcFontSize + 43.47352
-      );
-
-      setPredictedChars(predictedChars);
     }
   };
 
@@ -156,64 +120,5 @@ function App() {
       </div>
     </div>
   );
-}
-{
-  // <div className="w-[90%]">
-  //   <div className="form-control w-full">
-  //     <div className="input-group w-full justify-center ">
-  //       <div class="dropdown-top dropdown ">
-  //         <label
-  //           tabindex="0"
-  //           class="btn btn-ghost rounded-btn aspect-square w-[10vw] bg-slate-800  text-white focus:bg-slate-800"
-  //         >
-  //           🚀
-  //         </label>
-  //         <div
-  //           tabindex="0"
-  //           class="card-compact card dropdown-content  w-64  bg-primary  text-primary-content shadow"
-  //         >
-  //           <div class="card-body">
-  //             <h3 class="card-title">Card title!</h3>
-  //             <p>you can use any element as a dropdown.</p>
-  //           </div>
-  //         </div>
-  //       </div>
-  //       {/* <button className="b-0 btn aspect-square w-12 border-slate-800 bg-slate-800 text-xl text-white focus:bg-slate-800 focus:outline-none">
-  //         🚀
-  //       </button> */}
-  //       {/* <div className="dropdown-top dropdown">
-  //         <label
-  //           tabIndex={0}
-  //           className="b-0 btn aspect-square bg-slate-800 outline-none focus:bg-slate-800 focus:outline-none"
-  //         >
-  //           🚀
-  //         </label>
-  //         <div
-  //           tabIndex={0}
-  //           className="card dropdown-content dropdown-start menu rounded-box card-compact h-[20vh] w-[60vw] bg-slate-800 p-2 shadow "
-  //         ></div> */}
-  //       {/* <ul tabIndex={0} className="dropdown-content menu w-52 p-2 shadow">
-  //           <li>
-  //             <a>item 1</a>
-  //           </li>
-  //           <li>
-  //             <a>item 2</a>
-  //           </li>
-  //         </ul> */}
-  //       {/* </div> */}
-  //       <input
-  //         type="text"
-  //         placeholder="Bullet"
-  //         className="w-full  bg-slate-800 px-2 text-white focus:outline-none"
-  //       />
-  //       <button
-  //         className="w-30 btn btn-square border-none bg-slate-800
-  //       outline-none  focus:bg-slate-800"
-  //       >
-  //         <Send className="fill-slate-500 p-1" />
-  //       </button>
-  //     </div>
-  //   </div>
-  // </div>
 }
 export default App;
